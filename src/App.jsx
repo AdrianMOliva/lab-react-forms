de215986064ae415335106ed8b795bd0bc663589
Adrian Martin <martinolivaadrian@gmail.com>
2024-05-09 20:35:55 +0200

    Iterations 1-3 done

--- src/App.jsx
+++ src/App.jsx
@@ -8,41 +8,94 @@ import studentsData from "./assets/students.json";
 
 function App() {
   const [students, setStudents] = useState(studentsData);
+  const [fullName, setFullName] = useState("");
+  const [phone, setPhone] = useState("");
+  const [email, setEmail] = useState("");
+  const [graduated, setGraduated] = useState(false);
+  const [program, setProgram] = useState("");
+  const [profileImage, setProfileImage] = useState("");
+  const [graduationYear, setGraduationYear] = useState(2023);
 
+  const handleFullName = (e) => setFullName(e.target.value);
+  const handlePhone = (e) => setPhone(e.target.value);
+  const handleEmail = (e) => setEmail(e.target.value);
+  const handleGraduated = (e) => setGraduated(e.target.checked);
+  const handleProgram = (e) => setProgram(e.target.value);
+  const handleProfileImage = (e) => setProfileImage(e.target.value);
+  const handleGraduationYear = (e) => setGraduationYear(e.target.value);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const studentForm = {
+      fullName: fullName,
+      phone: phone,
+      email: email,
+      graduated: graduated,
+      program: program,
+      profileImage: profileImage,
+      graduationYear: graduationYear,
+    };
+    const updatedStudents = [studentForm, ...students];
+    setStudents(updatedStudents);
+  };
 
   return (
     <div className="App pt-20">
       <Navbar />
 
       {/* FORM */}
-      <form>
+      <form onSubmit={handleSubmit}>
         <span>Add a Student</span>
         <div>
           <label>
             Full Name
-            <input name="fullName" type="text" placeholder="Full Name" />
+            <input
+              name="fullName"
+              type="text"
+              placeholder="Full Name"
+              value={fullName}
+              onChange={handleFullName}
+            />
           </label>
 
           <label>
             Profile Image
-            <input name="image" type="url" placeholder="Profile Image" />
+            <input
+              name="image"
+              type="url"
+              placeholder="Profile Image"
+              value={profileImage}
+              onChange={handleProfileImage}
+            />
           </label>
 
           <label>
             Phone
-            <input name="phone" type="tel" placeholder="Phone" />
+            <input
+              name="phone"
+              type="tel"
+              placeholder="Phone"
+              value={phone}
+              onChange={handlePhone}
+            />
           </label>
 
           <label>
             Email
-            <input name="email" type="email" placeholder="Email" />
+            <input
+              name="email"
+              type="email"
+              placeholder="Email"
+              value={email}
+              onChange={handleEmail}
+            />
           </label>
         </div>
 
         <div>
           <label>
             Program
-            <select name="program">
+            <select name="program" value={program} onChange={handleProgram}>
               <option value="">-- None --</option>
               <option value="Web Dev">Web Dev</option>
               <option value="UXUI">UXUI</option>
@@ -60,24 +113,28 @@ function App() {
               maxLength={4}
               min={2023}
               max={2030}
+              value={graduationYear}
+              onChange={handleGraduationYear}
             />
           </label>
 
           <label>
             Graduated
-            <input name="graduated" type="checkbox" />
+            <input
+              name="graduated"
+              type="checkbox"
+              checked={graduated}
+              onChange={handleGraduated}
+            />
           </label>
 
           <button type="submit">Add Student</button>
         </div>
-
       </form>
       {/* FORM END */}
 
-
       {/* TABLE/LIST HEADER */}
       <TableHeader />
-
 
       {/* STUDENT LIST */}
       {students &&
